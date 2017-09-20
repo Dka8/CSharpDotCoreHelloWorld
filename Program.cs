0f22826827ea86d63269150b680a26963d1617be
wave <wave_hos@mail.ru>
2017-09-20 20:41:41 +0300

    new console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpDotCoreHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
